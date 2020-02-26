e5f7ae26bbd956e55b2c99dace745df913552f2c
Teo Winkler <teo.winkler@gmail.com>
2020-02-26 19:23:36 +0100

    fix absolute path to main.js

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# run to set permission > chmod 0755 deploy.sh
+
+# abort on errors
+set -e
+
+# navigate to gh-pages branch
+git checkout gh-pages
+
+# sync with master
+git merge master
+
+# add CNAME for custom domain
+
+# push to remote origin
+git push origin gh-pages
+
+
+cd -
